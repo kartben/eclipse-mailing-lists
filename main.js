cdebf2b7be9a78ccd5e92dc7bbd619a57ffde146
Benjamin Cabé <benjamin@eclipse.org>
2016-04-25 19:22:04 +0200

    fix matching for lists with 1 member (i.e. not memberS)

--- main.js
+++ main.js
@@ -75,8 +75,8 @@ function computeMailingListsStats(mailmanList) {
             if (!error && response.statusCode == 200) {
                 for (var project in result.projects) {
                     var nodes = select("//x:tr/x:td//x:strong[text() = '" + result.projects[project].dev_list.name + "']/text()/ancestor::x:tr/x:td[2]/text()", doc);
-
-                    mailing_lists[project] = parseInt(nodes[0].nodeValue.match(/(\d+) members/)[1])
+              //   console.log ( project , result.projects[project].dev_list.name,  "\n");
+                    mailing_lists[project] = parseInt(nodes[0].nodeValue.match(/(\d+) members?/)[1])
                 }
                 process.stdout.write(".");
                 callback();
